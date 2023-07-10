fa055b84eb14c4a560ffe560141195ede4564a1b
Chris Wu <wu.christophert@gmail.com>
2023-07-10 15:18:42 -0700

    started tests for center parser

--- api/spec/models/center_spec.rb
+++ api/spec/models/center_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Center, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:district) }
   end
+
+  describe 'parses date string' do
+    let!(:center) { create(:center) }
+    it 'correctly breaks down string into useable info' do
+      time      = '0/06:00-11:00'
+      # time_list = '0/06:00-11:00, 0/13:00-19:59'
+
+      expect().to eq()
+    end
+  end
 end
