11b0096f014c0f3e41bcac69573e197e93c27dce
Chris Wu <wu.christophert@gmail.com>
2023-07-10 15:40:07 -0700

    parse_date first pass

--- api/app/models/center.rb
+++ api/app/models/center.rb
@@ -2,4 +2,22 @@ class Center < ApplicationRecord
   belongs_to :district
 
   validates :name, :rating, presence: true
+
+  def self.parse_date(date)
+    day, time_range = date.split('/')
+
+    day_number = day.to_i
+
+    start, close = time_range.split('-')
+
+    opening_time = start.gsub(':','').to_i
+    closing_time = close.gsub(':','').to_i
+
+    {
+      day: day_number,
+      open: opening_time,
+      closed: closing_time
+    }
+
+  end
 end
--- api/spec/models/center_spec.rb
+++ api/spec/models/center_spec.rb
@@ -14,7 +14,43 @@ RSpec.describe Center, type: :model do
       time      = '0/06:00-11:00'
       # time_list = '0/06:00-11:00, 0/13:00-19:59'
 
-      expect().to eq()
+      # time_hash = [
+      #   {
+      #     day: 0,
+      #     open: 600,
+      #     closed: 1100
+      #   },
+      #   {
+        #     day: 0,
+        #     open: 1300,
+      #     closed: 1959
+      #   }
+      # ]
+
+      time_hash =
+        {
+          day: 0,
+          open: 600,
+          closed: 1100
+        }
+
+
+      expect(Center.parse_date(time)).to eq(time_hash)
     end
+    # it 'parses incorrect data into a useable format' do
+    #   time      = '-1/-'
+    #   time      = '0/-'
+    #   # time_list = '0/06:00-11:00, 0/13:00-19:59'
+
+    #   time_hash = [
+    #     {
+    #       day: 0,
+    #       open: 600,
+    #       closed: 1100
+    #     }
+    #   ]
+
+    #   expect().to eq()
+    # end
   end
 end
